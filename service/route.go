19579c35405f41bc95bf9be9c69a2f4ce2f98bf5
Bharat Kathi <bharat1031@gmail.com>
2024-05-10 12:46:45 -0700

    update local store init

--- service/route.go
+++ service/route.go
@@ -14,3 +14,28 @@ func GetAllRoutes() []model.Route {
 func GetNumRoutes() int {
 	return len(database.Local.Routes)
 }
+
+func GetRouteByID(id string) model.Route {
+	var route model.Route
+	for _, r := range database.Local.Routes {
+		if r.Route == id {
+			route = r
+			break
+		}
+	}
+	return route
+}
+
+func GetRoutesByServiceName(name string) []model.Route {
+	routes := make([]model.Route, 0)
+	for _, r := range database.Local.Routes {
+		if r.ServiceName == name {
+			routes = append(routes, r)
+		}
+	}
+	return routes
+}
+
+func CreateRoute(route model.Route) {
+	database.Local.Routes = append(database.Local.Routes, route)
+}
