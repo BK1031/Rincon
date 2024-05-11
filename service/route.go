b7693c4405afbfc54745a58eb1a9201a70639c29
Bharat Kathi <bharat1031@gmail.com>
2024-05-11 11:54:50 -0700

    build route graph

--- service/route.go
+++ service/route.go
@@ -6,6 +6,7 @@ import (
 	"rincon/database"
 	"rincon/model"
 	"rincon/utils"
+	"strings"
 	"time"
 )
 
@@ -49,12 +50,18 @@ func CreateRoute(route model.Route) error {
 	}
 	route.ServiceName = utils.NormalizeName(route.ServiceName)
 	route.CreatedAt = time.Now()
+	defer BuildRouteGraph()
 
-	if GetRouteByID(route.Route).Route != "" && route.ServiceName != GetRouteByID(route.Route).ServiceName {
-		if config.OverwriteRoutes == "true" {
-			DeleteRoute(route.Route)
+	if GetRouteByID(route.Route).Route != "" {
+		if route.ServiceName != GetRouteByID(route.Route).ServiceName {
+			if config.OverwriteRoutes == "true" {
+				DeleteRoute(route.Route)
+			} else {
+				return fmt.Errorf("route with id %s already exists", route.Route)
+			}
 		} else {
-			return fmt.Errorf("route with id %s already exists", route.Route)
+			utils.SugarLogger.Debugf("route with id %s for service %s already exists", route.Route, route.ServiceName)
+			return nil
 		}
 	}
 	database.Local.Routes = append(database.Local.Routes, route)
@@ -66,6 +73,60 @@ func DeleteRoute(id string) {
 		if r.Route == id {
 			database.Local.Routes = append(database.Local.Routes[:i], database.Local.Routes[i+1:]...)
 			break
+		}
+	}
+	BuildRouteGraph()
+}
+
+func BuildRouteGraph() {
+	children := make(map[string][]model.RouteNode)
+	for _, r := range database.Local.Routes {
+		slugs := strings.Split(r.Route, "/")
+		parent := ""
+		for i := 0; i < len(slugs); i++ {
+			if slugs[i] != "" {
+				if parent == "" {
+					parent = "/"
+				}
+				println("i: ", i)
+				println("len: ", len(slugs))
+				println("Parent: ", parent)
+				if _, exists := children[parent]; !exists {
+					children[parent] = make([]model.RouteNode, 0)
+				}
+				println("Slug: ", slugs[i])
+				// delete existing node
+				for j, n := range children[parent] {
+					if n.Path == slugs[i] {
+						children[parent] = append(children[parent][:j], children[parent][j+1:]...)
+						break
+					}
+				}
+				name := ""
+				if i == len(slugs)-1 {
+					name = r.ServiceName
+				}
+				children[parent] = append(children[parent], model.RouteNode{
+					ID:          parent + "/" + slugs[i],
+					Path:        slugs[i],
+					ServiceName: name,
+					CreatedAt:   time.Now(),
+				})
+				if parent == "/" {
+					parent += slugs[i]
+				} else {
+					parent += "/" + slugs[i]
+				}
+			}
+		}
+	}
+	println("=====================================")
+	println("Route Graph")
+	println("Nodes:", len(children))
+	for parent, nodes := range children {
+		println(parent)
+		for _, n := range nodes {
+			println(" -> " + n.Path + " (" + n.ServiceName + ")")
 		}
 	}
 }
