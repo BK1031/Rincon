5e7018c4c4ae247180cb590de746444bddd1f082
Bharat Kathi <bkathi@singlestore.com>
2024-07-31 12:09:06 -0700

    added route method validity checking

--- model/route.go
+++ model/route.go
@@ -2,8 +2,12 @@ package model
 
 import (
 	"rincon/config"
+	"slices"
+	"strings"
 	"time"
 )
+
+var validMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "*"}
 
 type Route struct {
 	Route       string    `json:"route" gorm:"primaryKey"`
@@ -16,9 +20,20 @@ func (Route) TableName() string {
 	return config.DatabaseTablePrefix + "route"
 }
 
+func (r *Route) IsMethodValid() bool {
+	methods := strings.Split(r.Method, ",")
+	for _, method := range methods {
+		if !slices.Contains(validMethods, method) {
+			return false
+		}
+	}
+	return true
+}
+
 type RouteNode struct {
 	ID          string    `json:"id" gorm:"primaryKey"`
 	Path        string    `json:"path"`
 	ServiceName string    `json:"service_name"`
+	Method      string    `json:"method"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime;precision:6"`
 }
